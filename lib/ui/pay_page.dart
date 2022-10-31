d4a2d9ba37a1c8585393c3ab3a9d9e68039e3fa7
Павел Никитин <p.doomkin@ya.ru>
2022-11-01 01:54:21 +0300

    Добавление кнопки оплаты техподдержики

--- lib/ui/pay_page.dart
+++ lib/ui/pay_page.dart
@@ -17,27 +17,48 @@ class PayPageState extends State<PayPage> {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    key: _scaffoldKey,
-    appBar: AppBar(
-      title: Text(L10n.support),
-    ),
-    body: InAppWebView(
-      initialData: InAppWebViewInitialData(
-        data: '''<HTML>
+  Widget build(BuildContext context) {
+    final org = bloc.activeOrg.valueWrapper?.value;
+    final startSupport = DateTime.now();
+    final endSupport = startSupport.add(const Duration(days: 365));
+    final description = 'Оплата техподдержки электронных табелей посещамости для ${org.name} ИНН ${org.inn} до ${dateToString(endSupport)}';
+
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: Text(L10n.support),
+      ),
+      body: InAppWebView(
+        initialData: InAppWebViewInitialData(
+            data: '''<HTML>
 <HEAD>
-<style>.tinkoffPayRow{display:block;margin:1%;width:160px;}</style>
+<style>
+  .tinkoffPayRow {
+    display:block;
+    margin:5%;
+    padding: 12px 12px;
+    border: 12px solid var(--input-border);
+    border-radius: 12px;
+    width:90%;
+    font-size: 3em;
+    font-family: sans-serif;
+  }
+  input[type="submit"] {
+    line-height: 60px;
+  }
+</style>
 <link rel="stylesheet" href="./html/payForm/static/css/t-widget.css" type="text/css">
-<script src="https://rest-api-test.tinkoff.ru/html/payForm/js/tinkoff.js"></script>
+<script src="https://securepay.tinkoff.ru/html/payForm/js/tinkoff_v2.js"></script>
 </HEAD>
 <BODY>
 <form name="TinkoffPayForm" onsubmit="pay(this); return false;">
-	<input class="tinkoffPayRow" type="hidden" name="terminalkey" value="1666686688281DEMO" >
+	<input class="tinkoffPayRow" type="hidden" name="terminalkey" value="1666686688281" >
 	<input class="tinkoffPayRow" type="hidden" name="frame" value="true">
 	<input class="tinkoffPayRow" type="hidden" name="language" value="ru">
-	<input class="tinkoffPayRow" type="text" placeholder="Сумма за год" name="amount" value="3000" required>
+	<input class="tinkoffPayRow" type="text" readonly placeholder="Сумма годовой техподдержки" name="amount" value="3000" required>
 	<input class="tinkoffPayRow" type="hidden" placeholder="Номер заказа" name="order">
-	<input class="tinkoffPayRow" type="hidden" placeholder="Описание заказа" name="description">
+	<input class="tinkoffPayRow" type="hidden" placeholder="Описание заказа" name="description" value="$description">
+	<textarea class="tinkoffPayRow" rows="4" readonly>$description</textarea>
 	<input class="tinkoffPayRow" type="hidden" placeholder="ФИО плательщика" name="name">
 	<input class="tinkoffPayRow" type="hidden" placeholder="E-mail" name="email">
 	<input class="tinkoffPayRow" type="hidden" placeholder="Контактный телефон" name="phone">
@@ -46,7 +67,8 @@ class PayPageState extends State<PayPage> {
 </BODY>
 </HTML>
 '''
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
